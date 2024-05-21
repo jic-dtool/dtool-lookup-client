5b5fd036a37a24104a56c46db9ce09f205cef363
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2024-05-21 11:03:39 +0200

    DOCS: rtd theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -85,7 +85,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Set the readthedocs theme.
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
